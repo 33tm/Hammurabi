62701e2d0c49797302b17ccabfbd0237320e3da9
33tm <80350771+33tm@users.noreply.github.com>
2024-06-16 16:00:31 -0700

    followers?

--- index.ts
+++ index.ts
@@ -1,39 +1,43 @@
-import crypto from "crypto"
+import { createHash, createHmac } from "crypto"
 
 const username = prompt("Enter username:")!.trim()
 const password = prompt("Enter password:")!.trim()
 
-const seed = crypto
-    .createHash("md5")
+const seed = createHash("md5")
     .update(username + username)
     .digest("hex")
 
-const device_id = `android-${crypto
-    .createHash("md5")
+const device_id = `android-${createHash("md5")
     .update(seed + "12345")
     .digest("hex")
     .slice(0, 16)}`
 
-const sign = (data: object) => `ig_sig_key_version=4&signed_body=${crypto
-    .createHmac("SHA256", "99e16edcca71d7c1f3fd74d447f6281bd5253a623000a55ed0b60014467a53b1")
-    .update(JSON.stringify(data))
-    .digest("hex")}.${encodeURIComponent(JSON.stringify(data))}`
+const sign = (data: object) => {
+    const body = createHmac("sha256", "46024e8f31e295869a0e861eaed42cb1dd8454b55232d85f6c6764365079374b")
+        .update(JSON.stringify(data))
+        .digest("hex")
+    return `ig_sig_key_version=4&signed_body=${body}.${encodeURIComponent(JSON.stringify(data))}`
+}
 
-const instagram = (path: string, data: object) => fetch(`https://i.instagram.com/api/v1${path}/`, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "Instagram 26.0.0.10.86 Android"
-    },
-    body: sign({
-        ...data,
-        username,
-        password,
-        device_id
-    })
-}).then(res => res.json())
+const instagram = async (method: "GET" | "POST", path: string, data?: object) => {
+    return await fetch(`https://i.instagram.com/api/v1${path}`, {
+        method,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Instagram 26.0.0.10.86 Android"
+        },
+        ...(method === "POST" && {
+            body: sign({
+                ...data,
+                username,
+                password,
+                device_id
+            })
+        })
+    }).then(res => res.json())
+}
 
-let login = await instagram("/accounts/login", {
+let login = await instagram("POST", "/accounts/login/", {
     phone_id: crypto.randomUUID(),
     guid: crypto.randomUUID()
 })
@@ -44,7 +48,7 @@ if (login.status === "fail" && login.error_type !== "two_factor_required") {
 }
 
 if (login.two_factor_required) {
-    login = await instagram("/accounts/two_factor_login", {
+    login = await instagram("POST", "/accounts/two_factor_login/", {
         verification_code: parseInt(prompt("Enter 2FA code:")!),
         two_factor_identifier: login.two_factor_info.two_factor_identifier
     })
@@ -55,8 +59,3 @@ if (login.two_factor_required) {
 }
 
 console.clear()
-
-await instagram(`/friendships/${login.logged_in_user.pk}/followers`, {
-    max_id: "0",
-    rank_token: crypto.randomUUID()
-}).then(console.log)
